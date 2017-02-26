6f6bf56941d037cecebb814141fc4cd8eff1626b
petermoserr <peterkmoser@gmail.com>
2017-02-26 15:25:20 -0500

    Server Optimization
    Changed most html files to PHP, also removed some unused css files

---
+++ RobotsPage.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<link rel="SHORTCUT ICON" HREF="photos/roboticslogo.png">
+	<title>Titans Robotics</title>
+	<link rel="stylesheet" href="index.css">
+	<!-- Your pages CSS stylesheet-->
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" href="menu-bar/menu-bar.css">
+	<!-- Menu bar's CSS stylesheet-->
+</head>
+
+<body>
+	<?php include ("header.html"); ?>
+	<div class="background panel panel-body b">
+		<h1 class="jumbotron title">Our Robots</h1>
+	</div>
+
+	<div class="panel-body b">
+		<img class="picture red" src="photos/2016Robot.jpg" style="float:left;width:300px;height:300px" ;>
+		<div class="panel-body background1 b">
+			<h1 class="c">2016</h1>
+			<p class="bl">This is our robot from the 2016 FRC game <u><a href="http://www.firstinspires.org/robotics/frc/welcome-to-first-stronghold" class="a">Stronghold.</a></u>The robot, named "Caligula" not only preformed well, but also allowed for the use of the robot
+				as an educational tool at science nights and club fairs. The robots low profile was designed with the intention of being able to quickly navigate obstacles on the course, and to be able to shoot both low and high goals.</p>
+		</div>
+	</div>
+	<div class="panel-body b">
+		<img class="picture blue" src="photos/2015Robot.jpg" style="float:left;width:300px;height:300px" ;>
+		<div class="panel-body background1 b">
+			<h1 class="c">2015</h1>
+			<p>This is our robot from the 2015 FRC game <u><a href="http://archive.usfirst.org/roboticsprograms/frc/2015-game" class="a">Recycle Rush.</a></u> As the first competion that team 5587 participated in, the robot named "Christopher" demonstrated the skill
+				and dedication of the newly formed team. The robot was able to stack and move totes, lift trash cans, and deposit "liter" (pool noodles) into trash cans stacked upon totes</p>
+		</div>
+	</div>
+	<iframe width="5" height="3" src="https://www.youtube.com/embed/Zu2S69sirs8" frameborder="0" allowfullscreen></iframe>
+</body>
