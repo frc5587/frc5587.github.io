534da151f5454dc21fcb8b54e3e8730e7476ce30
Brendan Doney <bre.doney@gmail.com>
2019-09-01 23:26:39 -0400

    Use single image naming convention and purge unused images

--- image_cleaner.py
+++ image_cleaner.py
@@ -2,11 +2,11 @@
     File name: image_cleaner.py
     Author: Brendan Doney
     Date created: 6/12/2018
-    Date last modified: 6/12/2018
+    Date last modified: 9/1/2019
     Python Version: 3.6.5
 
-    Be aware that this checker *DOES NOT INCLUDE* SCSS or CSS files, 
-    which may be used to set things such as background images
+    Be aware that this checker *DOES NOT LOOK IN* SCSS or CSS files,
+    which may contain image usage, such as with background images
 '''
 
 from os import listdir, walk
@@ -14,6 +14,8 @@ from os.path import isfile, join
 from html.parser import HTMLParser
 
 images_used = []
+blacklisted_img_files = [".DS_Store"]
+blacklisted_img_directories = ["photos/camps", "photos/Index-Images"]
 
 
 class MyHTMLParser(HTMLParser):
@@ -21,7 +23,8 @@ class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         """Method used my HTMLParser class to handle the parsing of tags (overriden method from base class)"""
 
-        if tag == "img" or tag == "source" or tag == "link" and attrs[0][1] == "icon":
+        if tag == "img" or tag == "source" or tag == "link" and attrs[0][
+                1] == "icon":
             # Iterate through all arguments to find src of image or link
             for word_set in attrs:
                 for i in range(len(word_set)):
@@ -43,7 +46,7 @@ for path in page_paths:
         file_path = join(path, file)
         if isfile(file_path):
             # Parse data from confirmed file now
-            print("Files in {}:".format(file_path))
+            print(f"Files in {file_path}:")
             with open(file_path, "r") as src_file:
                 parser.feed(src_file.read())
             print()
@@ -52,7 +55,9 @@ image_path = "photos/"
 all_images = []
 # Add all found images in specified photos directory to the all_images list
 for (dirpath, _, filenames) in walk(image_path):
-    all_images.extend([join(dirpath, str(file).strip()) for file in filenames if file not in all_images])
+    for file in filenames:
+        if file not in all_images and file not in blacklisted_img_files and dirpath not in blacklisted_img_directories:
+            all_images.append(join(dirpath, str(file).strip()))
 # print(all_images)
 
 images_used = set(images_used)
@@ -66,9 +71,8 @@ for image in images_used:
     elif image[0] == "/" and image[1:] in all_images:
         all_images.remove(image[1:])
     else:
-        print("Not found! \"{}\" in {}".format(image, all_images))
-        print()
+        print(f"Not found! '{image}'\n")
 
-# print("All images: {}".format(all_images))
-# print("Used images: {}".format(images_used))
-print("Images that are not needed: {}".format(all_images))
+# print(f"All images: {all_images}")
+# print(f"Used images: {images_used}")
+print(f"Images that are not needed: {all_images}")
