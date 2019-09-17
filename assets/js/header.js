083dee7d80f8ea27a938caea1a5bbe622cb52513
Brendan Doney <bre.doney@gmail.com>
2019-09-17 18:49:08 -0400

    Move images to assets folder and delete LICENSE.txt

---
+++ assets/js/header.js
@@ -0,0 +1,41 @@
+// Get the collapsible portion of the navbar and collapse button
+var collapsibleRegion = document.getElementById("header");
+var collapseButton = document.getElementsByClassName("navbar-toggle")[0];
+
+// Modal components
+var modal = document.getElementById("sponsor-modal"); // Sponsorship modal
+var btn = document.getElementById("supportBtn"); // Button that opens the modal
+var span = document.getElementsByClassName("close")[0]; // <span> element that closes the modal
+
+var pageContent = document.getElementsByTagName("main")[0];
+// const pageContent = document.getElementsByClassName("main")[0];
+
+function closeModal() {
+  modal.style.display = "none";
+  pageContent.style.overflow = "visible";
+}
+
+function openModal() {
+  // Check to ensure that the navbar is closed; otherwise, close it
+  if ($(collapsibleRegion).attr("aria-expanded")) {
+    $(collapseButton).click();
+  }
+  pageContent.style.overflow = "hidden";
+  // Now open the modal
+  modal.style.display = "flex";
+}
+
+// When the user clicks the button, open the modal
+btn.onclick = () => {
+  modal.style.display == "flex" ? closeModal() : openModal();
+};
+// When the user clicks on <span> (x), close the modal
+span.onclick = () => {
+  closeModal();
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = event => {
+  if (event.target == modal) {
+    closeModal();
+  }
+};
